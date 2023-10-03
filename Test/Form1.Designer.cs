8241af19addb37710d313c5351844325bf45faab
D3mahs <dima.kolmakov111@gmail.com>
2023-10-03 23:36:23 +0500

    #2 изменение кода

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(423, 258);
+            this.button1.Location = new System.Drawing.Point(291, 89);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(323, 88);
             this.button1.TabIndex = 0;
